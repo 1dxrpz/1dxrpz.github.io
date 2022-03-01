bd52268e79ef16bc279629f95d41a078d0297489
Kenneth Gibson <74268163+1dxrpz@users.noreply.github.com>
2022-03-01 09:43:34 +0300

    Delete scripts directory

--- scripts/index.js
+++
@@ -1,4 +0,0 @@
-document.body.addEventListener("wheel", _ => {
-	document.querySelector("header>nav").setAttribute("data-oncontent", 
-		document.querySelector("#content").getBoundingClientRect().top <= 0);
-});
