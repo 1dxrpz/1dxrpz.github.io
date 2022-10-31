456403092bdad2a856de5ab2923b2acf3ecf3dd7
1dxrpz <scaphoidederps@gmail.com>
2022-10-31 04:50:29 +0300

    updated resoulution scaler

--- scripts/search.js
+++ scripts/search.js
@@ -7,14 +7,15 @@ var searchHistory = {items:[]};
 var ShortcutLinks = [];
 
 const isValidUrl = urlString=> {
-	  	var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-	    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-	    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-	    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-	    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-	    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-	  return !!urlPattern.test(urlString);
-	}
+	var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+	'((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+	'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+	'(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+	'(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+	return !!urlPattern.test(urlString);
+}
+var maxSize = 5 * 1024 * 1024 / 24;
 function readFile() {
 	if (!this.files || !this.files[0]) return;
 	const reader = new FileReader();
@@ -24,15 +25,12 @@ function readFile() {
 		img.onload = function (event) {
 			var canvas = document.createElement("canvas");
 			var ctx = canvas.getContext("2d");
-			canvas.width = img.width / 2;
-			canvas.height = img.height / 2;
+			var r =  (img.height * img.width / 25) / maxSize;
+			r = r >= 1 ? r + 0.5 : r <= 0.2 ? r + 0.1 : r;
+			canvas.width = img.width / r;
+			canvas.height = img.height / r;
 			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-			ctx.mozImageSmoothingEnabled = false;
-			ctx.webkitImageSmoothingEnabled = false;
-			ctx.msImageSmoothingEnabled = false;
-			ctx.imageSmoothingEnabled = false;
 			var dataurl = canvas.toDataURL(imageFile.type);
-
 			document.querySelector("body").style.backgroundImage = `url(${dataurl})`;
 			localStorage.setItem("background", dataurl);
 		}
